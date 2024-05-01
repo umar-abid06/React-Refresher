0d125efb27d748e2584ebdce8d6595d17226a12f
Muhammad Umar Abid <mumarabid656@gmail.com>
2024-05-01 17:31:44 +0200

    ThemeSwitcher with Context in TailwindCSS

--- React_Basics/src/App.jsx
+++ React_Basics/src/App.jsx
@@ -4,6 +4,7 @@ import PasswordGenerator from "./components/PasswordGenerator";
 import CurrencyConverter from "./components/CurrencyConverter";
 import ReactRouter from "./components/ReactRouter";
 import ReactContext from "./components/ReactContext";
+import ThemeSwitcher from "./components/ThemeSwitcher";
 
 const App = () => {
   return (
@@ -12,7 +13,8 @@ const App = () => {
       {/* <PasswordGenerator /> */}
       {/* <CurrencyConverter /> */}
       {/* <ReactRouter /> */}
-      <ReactContext />
+      {/* <ReactContext /> */}
+      <ThemeSwitcher />
     </>
   );
 };
